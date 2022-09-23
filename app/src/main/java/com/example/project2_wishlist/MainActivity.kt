e52e26f8c4217332f41a18acd3e5554cbb21c8a9
Hello-ImNew <baokhanhtk129@gmail.com>
2022-09-23 16:21:51 -0400

    Second Commit

--- app/src/main/java/com/example/project2_wishlist/MainActivity.kt
+++ app/src/main/java/com/example/project2_wishlist/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.project2_wishlist
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -20,11 +22,19 @@ class MainActivity : AppCompatActivity() {
         val adapter =ItemAdapter(items)
         itemsRv.adapter = adapter
         itemsRv.layoutManager = LinearLayoutManager(this)
+        fun closeKeyBoard() {
+            val view = this.currentFocus
+            if (view != null) {
+                val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(view.windowToken, 0)
+            }
+        }
         findViewById<Button>(R.id.SubmitButton).setOnClickListener {
             val name = nameEdit.text.toString()
             var price = priceEdit.text.toString()
             val url = urlEdit.text.toString()
             if (name.isNotEmpty() && price.isNotEmpty() && url.isNotEmpty()) {
+                closeKeyBoard()
                 price = String.format("%.2f",price.toFloat())
                 nameEdit.text.clear()
                 priceEdit.text.clear()
